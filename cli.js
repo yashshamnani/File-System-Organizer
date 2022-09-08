f5d615a014434a0a2a29e914d247762ff5c65d4f
Yash Shamnani <95028496+yashshamnani@users.noreply.github.com>
2022-09-08 17:39:47 +0530

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,31 +1,4 @@
-//1.take input
-//nodejs cli.js help     
-//nodejs cli.js tree
-//nodejs cli.js organize
-// let path = require("path")
-// let helpobj = require("C://Users//LENOVO//OneDrive//Desktop//web4//11may_FileSystemOrganizer//commands//help.js")
-// let organizeobj = require(path.join(process.cwd(),"commands","organize.js"))
-// let treeobj = require(path.join(process.cwd(),"commands","tree.js"))
-// let arr = process.argv.slice(2)
-// let dirpath = arr[1]
  
-// if(arr[0]=='help')
-// {
-//     helpobj.helpfn();
-// }
-// else if(arr[0]=='tree')
-// {
-//      treeobj.treefn(dirpath)
-// }
-// else if(arr[0]=='organize')
-// {
-//     organizeobj.organizefn(dirpath);
-// }
-// else{
-//     console.log("wrong command . type help to see all commands")
-// }
-//////////////////////////////////////////////
-
 let input = process.argv.slice(2);
 let exec = input[0];
 let dirpath = input[1];
